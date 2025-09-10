dcf3daff3a57547ce1d9a8f22a8804f5de486509
Andiq7 <2214@htl.rennweg.at>
2025-09-10 20:38:56 +0200

    Kapitel 3 beendet

--- UE_00_Python_Kurs/Kapitel 3/Kapitel 3.py
+++ UE_00_Python_Kurs/Kapitel 3/Kapitel 3.py
@@ -1,2 +1,19 @@
 __author__ ="Andrija Pavlovic"
 __doc__ ="UE_00_Kapitel_3_Datentypen"
+#Frage 1
+m = 2.68
+a = 0.5
+
+ergebnis = (m/pow(a, 3))
+print(ergebnis, "\n")
+
+#Frage 2
+lösungswort = str(ergebnis)
+print(lösungswort)
+print(type(lösungswort), "\n")
+
+#Frage 3
+t = 5.5
+Q = 64.5
+I = Q/t
+print(I, "A\n")
