513b26e9aed0dcb6d0c9824b21aba554a8055a6a
Andiq7 <2214@htl.rennweg.at>
2025-09-11 16:30:06 +0200

    Kapitel 9 erstellt und erfolgreich beendet

---
+++ UE_00_Python_Kurs/Kapitel 9/Kapitel 9.py
@@ -0,0 +1,10 @@
+#Frage 1
+d = {'Bayern': 'München', 'Hessen': 'Wiesbaden'}
+print(d, '\n')
+
+#Frage 2
+print('Hessen' in d, "\n")
+
+#Frage 3
+d = {'Lisa': 14, 'Laura': 25, 'Leon': 15, 'Leonard': 17, 'Louisa': 15, 'Lena': 13 }
+print(d['Lena'])
