21512e51cddb989608c37bf161bedd2f90e84f41
Andiq7 <2214@htl.rennweg.at>
2025-09-11 11:14:57 +0200

    Kapitel 7 beendet

---
+++ UE_00_Python_Kurs/Kapitel 7/Kapitel 7.py
@@ -0,0 +1,12 @@
+#Frage 1
+"""def ausgabe(text, zähler):
+    for i in range(zähler)
+        print(text)
+ausgabe('Du hast das 7. Kapitel erfolgreich abgeschlossen - Super!', 10)
+"""
+
+#Frage 2
+def ausgabe(text, zähler):
+    for i in range(zähler):
+        print(text)
+ausgabe('Du hast das 7. Kapitel erfolgreich abgeschlossen - Super!', 10)
