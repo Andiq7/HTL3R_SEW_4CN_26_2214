98f91a1b297b834216ac9bf477c7d85545dc476e
Andrija Pavlovic <andrija.pavlovic@htl.rennweg.at>
2025-09-25 21:40:10 +0200

    Die Liste  m_list erstellt

--- UE_01_Py_Intro/rekursiv.py
+++ UE_01_Py_Intro/rekursiv.py
@@ -1,11 +1,18 @@
 __author__ = "Andrija Pavlovic"
 
+
 def M(n):
     if n > 100:
         return n - 10
     else:
         return M(M(n + 11))
 
+
 if __name__ == "__main__":
     print(M(99))
     print(M(100), '\n')
+
+    m_list = []
+    for n in range(200):
+        m_list.append(M(n))
+    print(m_list)
