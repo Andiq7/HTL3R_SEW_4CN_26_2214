7557c45f80cd9eef9624e87b98e27420245f3b0c
Andrija Pavlovic <andrija.pavlovic@htl.rennweg.at>
2025-09-25 21:52:06 +0200

    Frage beantwortet und die Berechnungszeit eingebaut

--- UE_01_Py_Intro/rekursiv.py
+++ UE_01_Py_Intro/rekursiv.py
@@ -1,3 +1,5 @@
+from time import time
+
 __author__ = "Andrija Pavlovic"
 
 
@@ -12,12 +14,23 @@ if __name__ == "__main__":
     print(M(99))
     print(M(100), '\n')
 
+    t0 = time()
     m_list = []
     for n in range(200):
         m_list.append(M(n))
     print(m_list)
+    t1 = time()
+    print("Dauer Liste: ", t1 - t0, "Sekunden")
 
+    t2 = time()
     m_dict = {}
     for n in range(200):
         m_dict[n] = M(n)
     print(m_dict)
+    t3 = time()
+    print("Dauer Dict: ", t3 - t2, "Sekunden")
+
+    # Bemerkenswert an der Funktion M(n) (McCarthy-91-Funktion) ist, dass für
+    # alle Werte von n ≤ 100 das Ergebnis immer 91 ist. Nur für Werte von n > 100
+    # liefert die Funktion n - 10 zurück. Das zeigt, wie Rekursion zu einem konstanten
+    # Ergebnis für einen großen Wertebereich führen kann.
